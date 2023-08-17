1aa2f9b81021319f7de3d85a9c731bf4cfdc301e
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2023-08-17 10:36:38 +0700

    new

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -2,9 +2,9 @@ module.exports = {
     DBConnectors: {
         host: process.env.DB_HOST || "db4free.net",
         port: process.env.DB_PORT || 3306,
-        username: process.env.DB_USER || "audiobook",
-        password: process.env.DB_PASSWORD || "thanhbinh88",
-        database: process.env.DB_NAME || "audiobook",
+        username: process.env.DB_USER || "thainguyen",
+        password: process.env.DB_PASSWORD || "Nguyen1222004",
+        database: process.env.DB_NAME || "project",
         dialect: process.env.DB_DIALECT || "mysql",
     },
     jwtAuthKey: 'abcxyz',
