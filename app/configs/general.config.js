e9da9800488de8194022dcc0c1560905e204678a
LAPTOP-5SG2CNP0\NGUYEN <thainguyen122004@gmail.com>
2023-08-17 19:48:02 +0700

    new

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -4,7 +4,7 @@ module.exports = {
         port: process.env.DB_PORT || 3306,
         username: process.env.DB_USER || "thainguyen",
         password: process.env.DB_PASSWORD || "Nguyen1222004",
-        database: process.env.DB_NAME || "project",
+        database: process.env.DB_NAME || "projectapi",
         dialect: process.env.DB_DIALECT || "mysql",
     },
     jwtAuthKey: 'abcxyz',
